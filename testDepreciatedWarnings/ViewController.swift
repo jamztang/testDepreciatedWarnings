85e0d2c88b69f10439ab833c1517629b506bf9f0
James Tang <james@goodnotesapp.com>
2019-09-03 15:17:59 +0800

    Add guard depreciated API

--- testDepreciatedWarnings/ViewController.swift
+++ testDepreciatedWarnings/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let menu = UIMenuController()
+        
+        if #available(iOS 13.0, *) {
+            print("iOS 13")
+        } else {
+            menu.setTargetRect(.zero, in: view)
+        }
     }
 
 
